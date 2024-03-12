25e6fb8c7fb3f6b77d6ac7173a6df670dfcd801d
meghagavankar1 <meghagavankar@nimapinfotech.com>
2024-03-12 17:23:29 +0530

    some code

--- my-project/src/app/home/home.component.ts
+++ my-project/src/app/home/home.component.ts
@@ -10,21 +10,23 @@ import { distinctUntilChanged, skip } from 'rxjs';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements  OnInit{
+  
   dataSource!: homeDataSource;
   pendingResult:any
   displayedColumns= ["BranchFDGLCode", "BranchName", "CustomerID","CustomerName","RedemptionID","RedemptionDate&Time","TotalPrice","RoundOff","TotalPayable","Status"];
   constructor(private service:HomeService){}
 
   ngOnInit(){
+    
     this.dataSource = new homeDataSource(this.service);
-    const entitiesSubscription = this.dataSource.entitySubject
-      .pipe(skip(1), distinctUntilChanged())
-      .subscribe((res:  any[]) => {
-        this.pendingResult = res;
-        console.log(this.pendingResult)
+    // const entitiesSubscription = this.dataSource.entitySubject
+    //   .pipe(skip(1), distinctUntilChanged())
+    //   .subscribe((res:  any[]) => {
+    //     this.pendingResult = res;
+    //     console.log(this.pendingResult)
        
-      });
-    // this.subscriptions.push(entitiesSubscription);
+    //   });
+    
     this.dataSource.getAllData();
 
   }
